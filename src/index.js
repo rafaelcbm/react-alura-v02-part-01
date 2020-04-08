64be7dbec7555334540b7673aab0026a752df6c7
Rafael Correia <rafaelcbm@gmail.com>
2020-04-08 14:46:21 -0300

    Tratamento de erros na API Organização do código

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './Pages/Home/Home';
+import Sobre from './Pages/Sobre/Sobre';
+import Livros from './Pages/Livros/Livros';
+import Autores from './Pages/Autores/Autores';
+import NotFound from './Pages/NotFound/NotFound';
 
-import Autores from './Autores';
-import Livros from './Livros';
-import Sobre from './Sobre';
-import NotFound from './NotFound'
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
-            <Route path="/" exact={true} component={App} />
-            <Route path="/sobre" component={Sobre} />
-            <Route path="/livros" component={Livros} />
-            <Route path="/autores" component={Autores} />
+            <Route path='/' exact={true} component={Home} />
+            <Route path='/sobre' component={Sobre} />
+            <Route path='/livros' component={Livros} />
+            <Route path='/autores' component={Autores} />
             <Route component={NotFound} />
+
         </Switch>
-    </BrowserRouter>, document.getElementById('root'));
+    </BrowserRouter>
+    , document.getElementById('root'));
 
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
